0450d69118d1fc67efcda39e3165e1eb71a118df
FAVNakaihideaki <nakaihideaki47@gmail.com>
2025-12-11 18:06:13 +0900

    fix case-sensitive paths for domRenderer and gameState

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ import {
   updateButtons,
   renderStats,
   loadGuestStats,
-} from './ui/dom/ui.js';
+} from './ui/ui.js';
 
 import { GameState } from './core/gameState.js';
 import { renderHands } from './ui/renderer.js';
